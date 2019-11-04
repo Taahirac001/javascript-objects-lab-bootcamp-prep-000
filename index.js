1ade6a885cf642fa9cdb455b614489f922df0ed4
Taahira Carter <taahirac001@gmail.com>
2019-11-04 02:58:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   var deleteNewObj = Object.assign({}, Object);
   delete deleteNewObj[key];
+  return deleteNewObj;
 }
 
 
