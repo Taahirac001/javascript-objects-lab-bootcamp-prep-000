aa1cc79e3fabe7c3265de76681703ed93a6628aa
Taahira Carter <taahirac001@gmail.com>
2019-11-04 02:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function updateObjectWithKeyAndValue(object, key, value){
   var newObject = Object.assign({}, object);
-  newObject[key] = value
-  return object, newObject
+  newObject[key] = value;
+  return object, newObject;
 }
 
-
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
 
 
 
